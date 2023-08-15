1fd811302a2187d187fa781c9a46f3eb3bf20708
paipragathi <102676002+paipragathi@users.noreply.github.com>
2023-08-15 18:07:18 +0000

    newranch

--- src/App.jsx
+++ src/App.jsx
@@ -28,25 +28,83 @@
 // export default App;
 
 
-import React, { useEffect, useState } from 'react'
+// import React, { useEffect, useState } from 'react'
+// import Landing from './landing site/landing';
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import Dashboard from './patient/Dashboard';
+// import Appointment from './patient/Appointment';
+// import PatientLogin from './patient/PatientLogin';
+// import PatientSignup from './patient/PatientSignup';
+// import Appointment2 from './doctor/Appointment2';
+
+// const App = () => {
+
+//   return (
+//     <div>
+      
+//     <Router>
+//       <Routes>
+//             <Route path="/patient" element={<Dashboard/>} /> 
+//             <Route path="/" element={<Landing/>} />
+//             <Route path="/appointment" element={<Appointment/>} /> 
+//             <Route path='/patient/login' element={<PatientLogin/>} />
+//             <Route path='/patient/signup' element={<PatientSignup/> } />
+          
+//             <Route path="/document" element={<Appointment2/>} /> 
+
+//       </Routes>
+//     </Router>
+    
+
+
+//     </div>
+    
+//   )
+// }
+
+// export default App;
+
+// import React from 'react'
+// import Landing from './landing site/landing';
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import Dashboard from './patient/Dashboard';
+// import Appointment2 from './doctor/Appointment2';
+// import Appointment from './patient/Appointment';
+// const App = () => {
+//   return (
+//     <div>
+      
+//     <Router>
+//       <Routes>
+//             <Route path="/patient" element={<Dashboard/>} /> 
+//             <Route path="/" element={<Landing/>} />
+//             <Route path="/appointment" element={<Appointment/>} /> 
+//             <Route path="/document" element={<Appointment2/>} /> 
+
+//       </Routes>
+//     </Router>
+    
+
+
+//     </div>
+    
+//   )
+// }
+
+// export default App;
+
+
+import React from 'react'
 import Landing from './landing site/landing';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Dashboard from './patient/Dashboard';
 import Appointment from './patient/Appointment';
 import PatientLogin from './patient/PatientLogin';
 import PatientSignup from './patient/PatientSignup';
-import Appointment2 from './doctor/Appointment2';
+import FileUpload from './patient/document';
 
 const App = () => {
 
-  const [account ,setAccount] = useState("");
-  const [ehrContract , setEhrContract] = useState(null);
-  const [rewardContract , setRewardContract] = useState(null);
-  const [provider , setProvider] = useState(null);
-
-  useEffect(()=>{
-    
-  }, [])
 
   return (
     <div>
@@ -59,7 +117,7 @@ const App = () => {
             <Route path='/patient/login' element={<PatientLogin/>} />
             <Route path='/patient/signup' element={<PatientSignup/> } />
           
-            <Route path="/document" element={<Appointment2/>} /> 
+            <Route path="/document" element={<FileUpload/>} /> 
 
       </Routes>
     </Router>
--- src/patient/Dashboard.jsx
+++ src/patient/Dashboard.jsx
@@ -1,103 +1,141 @@
+// import React from 'react'
+// // import SideNav from '../elements/Navbar'
+// // import PatientLogin from './PatientLogin'
+// // import s1 from "../assets/slider_1.jpg"
+// // import s2 from "../assets/slider_2.jpg"
+// //  import ResponsiveAppBar from '../elements/Navbar-2'
+// // import s3 from '../assets/slider_3.jpg'
+// // import SideNav from '../elements/Navbar'
+// import EHR from "../artifacts/contracts/EHR.sol/EHR.json";
+// import Reward from '../artifacts/contracts/Reward.sol/Reward.json';
+// import { ethers } from 'ethers'
+// import { useState } from 'react'
+// import { useEffect } from 'react'
+// import { SetMeal } from '@mui/icons-material'
+// import Sidebar from '../elements/sidebar';
+
+
+// const Dashboard = () => {
+
+//   const [account,setAccount] = useState("");
+//   const [ehrContract ,setEhrContract] = useState(null);
+//   const [rewardContract ,setRewardContract] = useState(null);
+//   const [provider ,setProvider] = useState(null);
+//   const [email ,setEmail] = useState("");
+
+//   useEffect(()=>{
+//     const provider = new ethers.providers.Web3Provider(window.ethereum);
+//     const loadProvider = async()=>{
+//       if(provider){
+//         window.ethereum.on("chainChanged", () => {
+//           window.location.reload();
+//         });
+
+//         window.ethereum.on("accountsChanged", () => {
+//           window.location.reload();
+//         });
+//         await provider.send("eth_requestAccounts",[]);
+//         const signer = provider.getSigner();
+//         const address = await signer.getAddress();
+//         setAccount(address);
+
+//         let ehrContractAddress = '0x897F6B5f3a44096AE8DE19f7f413b94370868A8a';
+//         let rewardContractAddress = '0x8824b9479f9C217f7BB28FBEaC728A1118e2B48C';
+
+//         const ehrContract = new ethers.Contract(
+//           ehrContractAddress, EHR.abi , signer
+//         );
+
+//         console.log(ehrContract);
+//         setEhrContract(ehrContract);
+        
+//         const rewardContract = new ethers.Contract(
+//           ehrContractAddress, EHR.abi,signer
+//         );
+//         console.log(rewardContract);
+//         setRewardContract(rewardContract);
+
+//         setProvider(provider);
+
+//       }else{
+//         console.error("Metamask not installed")
+//       }
+//     }
+//     provider && loadProvider();
+
+//     fetch('http://localhost:3000/patient/me', {
+//       method: 'GET',
+//       headers:{
+//         'Authorization': 'Bearer ' + localStorage.getItem('token')
+//       }
+//     }).then((res)=>{
+//       res.json().then((data)=>{
+//         setEmail(data.email)
+//       })
+//     })
+//   },[])
+
+//   const slides = [
+//     { url: s1},
+//     { url: s2 },
+//     {url: s3 },
+//   ];
+//   const containerStyles = {
+//     width: "1000px",
+//     height: "530px",
+//     margin: "0 0",
+    
+//   };
+    
+//   return (
+//     <> 
+  
+//       <Sidebar/>
+//     </>
+  
+//   )
+
+
+// };
+
+
+// export default Dashboard;
+
+
 import React from 'react'
 // import SideNav from '../elements/Navbar'
-import PatientLogin from './PatientLogin'
-import s1 from "../assets/slider_1.jpg"
-import s2 from "../assets/slider_2.jpg"
- import ResponsiveAppBar from '../elements/Navbar-2'
-import s3 from '../assets/slider_3.jpg'
-import SideNav from '../elements/Navbar'
-import EHR from "../artifacts/contracts/EHR.sol/EHR.json";
-import Reward from '../artifacts/contracts/Reward.sol/Reward.json';
-import { ethers } from 'ethers'
-import { useState } from 'react'
-import { useEffect } from 'react'
-import { SetMeal } from '@mui/icons-material'
 
-
+// import s1 from "../assets/slider_1.jpg"
+// import s2 from "../assets/slider_2.jpg"
+//  import ResponsiveAppBar from '../elements/Navbar-2'
+// import s3 from '../assets/slider_3.jpg'
+// import SideNav from '../elements/Navbar'
+import Sidebar from '../elements/sidebar';
 const Dashboard = () => {
 
-  const [account,setAccount] = useState("");
-  const [ehrContract ,setEhrContract] = useState(null);
-  const [rewardContract ,setRewardContract] = useState(null);
-  const [provider ,setProvider] = useState(null);
-  const [email ,setEmail] = useState("");
-
-  useEffect(()=>{
-    const provider = new ethers.providers.Web3Provider(window.ethereum);
-    const loadProvider = async()=>{
-      if(provider){
-        window.ethereum.on("chainChanged", () => {
-          window.location.reload();
-        });
-
-        window.ethereum.on("accountsChanged", () => {
-          window.location.reload();
-        });
-        await provider.send("eth_requestAccounts",[]);
-        const signer = provider.getSigner();
-        const address = await signer.getAddress();
-        setAccount(address);
-
-        let ehrContractAddress = '0x897F6B5f3a44096AE8DE19f7f413b94370868A8a';
-        let rewardContractAddress = '0x8824b9479f9C217f7BB28FBEaC728A1118e2B48C';
-
-        const ehrContract = new ethers.Contract(
-          ehrContractAddress, EHR.abi , signer
-        );
-
-        console.log(ehrContract);
-        setEhrContract(ehrContract);
-        
-        const rewardContract = new ethers.Contract(
-          ehrContractAddress, EHR.abi,signer
-        );
-        console.log(rewardContract);
-        setRewardContract(rewardContract);
-
-        setProvider(provider);
-
-      }else{
-        console.error("Metamask not installed")
-      }
-    }
-    provider && loadProvider();
-
-    fetch('http://localhost:3000/patient/me', {
-      method: 'GET',
-      headers:{
-        'Authorization': 'Bearer ' + localStorage.getItem('token')
-      }
-    }).then((res)=>{
-      res.json().then((data)=>{
-        setEmail(data.email)
-      })
-    })
-  },[])
-
-  const slides = [
-    { url: s1},
-    { url: s2 },
-    {url: s3 },
-  ];
-  const containerStyles = {
-    width: "1000px",
-    height: "530px",
-    margin: "0 0",
+  // const slides = [
+  //   { url: s1},
+  //   { url: s2 },
+  //   {url: s3 },
+  // ];
+  // const containerStyles = {
+  //   width: "1000px",
+  //   height: "530px",
+  //   margin: "0 0",
     
-  };
+  // };
     
   return (
-    <> 
-    <h1>helloxfhf</h1>
-    <ResponsiveAppBar/> 
-      <div className="c" style={containerStyles}>
-        <SideNav slides={slides} />
-      </div> 
-    </>
-  
-  )
+<> 
+{/* <h1>helloxfhf</h1> */}
+ {/* <ResponsiveAppBar/>  */}
+<Sidebar/>
+      {/* <div className="c" style={containerStyles}> */}
+        {/* <SideNav slides={slides} /> */}
+      {/* </div>  */}
+  </>
 
-
+)
 };
 
 export default Dashboard;
